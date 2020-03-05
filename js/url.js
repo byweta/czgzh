61454e2a247c6efe4a30ad1e82fb8b2471394a90
byweta <byweta@qq.com>
2020-03-05 09:29:58 +0800

    配置域名地址

--- js/url.js
+++ js/url.js
@@ -1,4 +1,5 @@
 // 主服务地址
-var mHost = "http://60.220.248.22:8182";
+// var mHost = "http://60.220.248.22:8182";
+var mHost = "http://www.czsjdchbjy.com:8182";
 // 主服务系统名称
 var mServerName = "/ljcc";
